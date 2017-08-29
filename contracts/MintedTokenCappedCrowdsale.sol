af11dbc96c5fed741e0f503fe07c073d2b24e7b6
viktor <baranov.viktor.27@gmail.com>
2017-08-29 19:09:05 +0300

    fix

--- contracts/MintedTokenCappedCrowdsale.sol
+++ contracts/MintedTokenCappedCrowdsale.sol
@@ -36,7 +36,7 @@ contract MintedTokenCappedCrowdsale is Crowdsale {
   function isBreakingInvestorCap(address addr, uint tokenAmount) constant returns (bool limitBroken) {
     uint multiplier = 10 ** token.decimals();
     uint maxCap = earlyParticipantWhitelist[addr].maxCap;
-    return maxCap.times(multiplier) > tokenAmountOf[addr].plus(tokenAmount);
+    return tokenAmountOf[addr].plus(tokenAmount) > maxCap.times(multiplier);
   }
 
   function isCrowdsaleFull() public constant returns (bool) {
