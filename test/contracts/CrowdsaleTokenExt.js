1d973ad6a28c9a9cd9732d219683d37f0fba46ed
viktor <baranov.viktor.27@gmail.com>
2017-12-18 13:49:29 +0300

    chai-as-promised module is deleted

--- test/contracts/CrowdsaleTokenExt.js
+++ test/contracts/CrowdsaleTokenExt.js
@@ -6,7 +6,6 @@ const ReservedTokensFinalizeAgent = artifacts.require("./ReservedTokensFinalizeA
 const constants = require("../constants");
 
 require('chai')
-  .use(require('chai-as-promised'))
   .use(require('chai-bignumber')(web3.BigNumber))
   .should();
 
